f561f2a49535246072641786e51f69b1c323a7a3
zakariaayl <ayougilzaki@gmail.com>
2025-01-24 19:12:22 +0100

    Initial commit

---
+++ apps/Ecommerce_front_back/jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  displayName: 'Ecommerce_front_back',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/apps/Ecommerce_front_back',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
